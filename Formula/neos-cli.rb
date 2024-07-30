02a79a6db107c299ef897112f0d5760b71ad829f
Bastian Bense <bb@neo.de>
2024-07-30 10:45:29 +0200

    chore: Update neos_cli formula to use version 0.7.1

--- Formula/neos-cli.rb
+++ Formula/neos-cli.rb
@@ -1,8 +1,8 @@
 class NeosCli < Formula
   desc "The neosphere CLI tool."
   homepage "https://neo.de"
-  url "https://github.com/neosw-oss/homebrew-tap/releases/download/0.7.0/sphere.tar.gz"
-  sha256 "307401f83628b4e0dab53a880ff5fb9e00c810702d31a26839cdd7aad2e4ff2d"
+  url "https://github.com/neosw-oss/homebrew-tap/releases/download/0.7.1/sphere.tar.gz"
+  sha256 "e6bf6974c6707b7b129b680ac645a6106392990980d266e3ca557742b6164050"
 
   def install
     bin.install "sphere"
